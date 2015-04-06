76ac29de14923eb80a97cb976363e88517c3f633
Tadhg <tadhger87@gmail.com>
2015-04-06 01:26:33 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user = users(:tadhg)
   end
   
   test "login with valid information followed by logout" do
