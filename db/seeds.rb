2a625d73948d75ba26e8fda5ef0a075bc22a1ac5
Piotr Szul <psmail82@gmail.com>
2015-04-08 19:36:14 +0000

    changes in seed.db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ User.create!(name:  "Tadhg Ó Cuirrín",
              activated: true,
              activated_at: Time.zone.now)
              
+<<<<<<< HEAD
              User.create!(name: "Piotr Szul",
                           email: "psmail82@gmail.com",
                           password: "pierdykam",
@@ -13,6 +14,8 @@ User.create!(name:  "Tadhg Ó Cuirrín",
                           admin: true,
                           activated: true,
                           activated_at: Time.zone.now)
+=======
+>>>>>>> 14409bd524ae61a0bd0fd4fcfad09d52a508d3f5
              
 
 99.times do |n|
@@ -31,4 +34,47 @@ User.create!(name:  "Tadhg Ó Cuirrín",
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+<<<<<<< HEAD
 end
+=======
+
+
+Item.create!(name: "Liv Envie",
+            description: "With its lightweight and durable ALUXX aluminium frame and upright flat-bar positioning, Alight is comfortable enough for a daily commute, but also agile and sporty for longer rides. Women’s-specific geometry and sizing gives Alight a stable, confident ride quality and it’s easy to personalise with racks and fenders to fit your riding lifestyle.",
+            price: 999.00,
+            image_url: "LivEnvie.jpg")
+          
+          
+            
+Item.create!(name: "FastRoad SLR",
+            description: "With an ultra-lightweight ALUXX SLR aluminium 
+            frameset and a fitness oriented riding position, FastRoad SLR is an 
+            all-new flat-bar road bike for daily fitness riding and commuting. 
+            The fastrolling 700c wheels and aero-shaped frame tubing offers a smooth,
+            efficient ride. The D-Fuse seatpost design helps smooth out rough roads and
+            the disc brakes give you smooth, confident braking power in all kinds of weather.",
+            price: 999.00,
+            image_url: "FastRoadSLR.jpg")
+            
+            Item.create!(name: "Envie Advanced 1",
+            description: "Built on years of research, development and testing by riders including the fastest female pro on the planet, Rabobank-Liv racer Marianne Vos, Envie Advanced redefines the meaning of fast. Liv’s 3F design philosophy is paired with cutting-edge aerodynamic innovations including AeroSystem Shaping technology and the SpeedControl SL brake system. The Advanced-grade composite frame is handcrafted with the OverDrive steerer tube for precise handling. It all adds up to the ultimate aero machine for road racing and triathlons.",
+            price: 2099.00,
+            image_url: "EnvieAdvanced.jpg")
+            
+            Item.create!(name: "Alight 2 ladies bicycle",
+            description: "With its lightweight and durable ALUXX aluminium frame and upright flat-bar positioning, Alight is comfortable enough for a daily commute, but also agile and sporty for longer rides. Women’s-specific geometry and sizing gives Alight a stable, confident ride quality and it’s easy to personalise with racks and fenders to fit your riding lifestyle.",
+            price: 499.00,
+            image_url: "Alight2.jpg")
+            
+            
+            
+            
+            Item.create!(name: "Avail",
+            description: "Thrive blends the speed and agility of a road bike with a more comfortable, flat-bar upright riding position. The all-new women’s-specific frame features Liv’s 3F design and lightweight ALUXX aluminium for a quick, agile feel. The D-Fuse seatpost reduces road vibrations and all-new disc-brake technology offers powerful braking, even in wet weather, so you can ride with supreme confidence.",
+            price: 849.00,
+            image_url: "Avail.jpg")
+
+
+
+
+>>>>>>> 14409bd524ae61a0bd0fd4fcfad09d52a508d3f5
