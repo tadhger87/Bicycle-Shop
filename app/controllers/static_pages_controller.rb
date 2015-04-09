ad33d74da5c81e7f4cfe7dd68dfe5378f0ae543f
Tadhg <tadhger87@gmail.com>
2015-04-09 13:38:14 +0000

    microposts feed on homepage with delete function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
-  def home
+  
+  
+def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
+  
 
   def help
   end
@@ -22,4 +29,5 @@ class StaticPagesController < ApplicationController
   
   def repairs
   end
+  
 end
