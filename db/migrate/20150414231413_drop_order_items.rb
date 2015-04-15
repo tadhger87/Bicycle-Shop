a12d4853f974e06e2fbfbb28838359e063d243da
Tadhg <tadhger87@gmail.com>
2015-04-15 19:22:06 +0000

    Messing with stuff

--- db/migrate/20150414231413_drop_order_items.rb
+++
@@ -1,5 +0,0 @@
-class DropOrderItems < ActiveRecord::Migration
-  def change
-    drop_table :order_items
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 20150415173131) do
 
   create_table "cart_items", force: :cascade do |t|
@@ -37,7 +34,6 @@ ActiveRecord::Schema.define(version: 20150415173131) do
 
   add_index "carts", ["user_id"], name: "index_carts_on_user_id"
 
-
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -45,12 +41,7 @@ ActiveRecord::Schema.define(version: 20150415173131) do
     t.string   "image_url"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "user_id"
-    t.integer  "order_id"
   end
-
-  add_index "items", ["order_id"], name: "index_items_on_order_id"
-  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
