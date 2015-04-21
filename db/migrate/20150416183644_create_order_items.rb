a2406b1f2deb5b351011b95cdd671b2b82a40692
Piotr Szul <psmail82@gmail.com>
2015-04-21 17:35:01 +0000

    create_products migration before create_order_item

--- db/migrate/20150416183644_create_order_items.rb
+++ db/migrate/20150416183644_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-      #t.references :product, index: true
+      t.references :product, index: true
       t.references :order, index: true
       t.decimal :unit_price, precision: 12, scale: 3
       t.integer :quantity
