b236b73c00a3c1d646511497fdb38a066bbbf5da
Piotr Szul <psmail82@gmail.com>
2015-04-22 10:12:48 +0000

    reference orders in cart

--- db/migrate/20150419004904_create_carts.rb
+++ db/migrate/20150419004904_create_carts.rb
@@ -2,10 +2,9 @@ class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
 
-      t.timestamps null: false#
-      t.references :orders, index: true
+      t.timestamps null: false
+      add_reference :orders, index: true
     end
-    
     add_foreign_key :carts, :orders
   end
 end
