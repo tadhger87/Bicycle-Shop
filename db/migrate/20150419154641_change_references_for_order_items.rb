ad05f67f6cd091fe40a92c5c386f67e41d30b567
Piotr Szul <psmail82@gmail.com>
2015-04-22 18:13:39 +0000

    comment out ChangeReferencesForOrderItems as this is fixed in AddColumsToOrder(1)

--- db/migrate/20150419154641_change_references_for_order_items.rb
+++ db/migrate/20150419154641_change_references_for_order_items.rb
@@ -1,9 +1,9 @@
-#class ChangeReferencesForOrderItems < ActiveRecord::Migration
-#  def change
-#    
+class ChangeReferencesForOrderItems < ActiveRecord::Migration
+def change
+    
 #    remove_column :order_items, :orders_id
 #    remove_column :order_items, :order_id_id
-#    
+    
 #    add_column :order_items, :order_id, :integer
-#  end
-#end
+end
+end
